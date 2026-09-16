702d15b6dd0c19d59b8b9730ab56295d07a039f4
Nguyễn Trường Phát <nguyentruongphat99999@gmail.com>
2026-09-16 14:59:04 +0700

    hoan thanh kiem tra va tach email an toan

--- bai_tap_chuoi.py
+++ bai_tap_chuoi.py
@@ -1,6 +1,9 @@
-email = input("khai bao email:")
-vi_tri_ten = email.find("@")
-ten_nguoi_dung = email[:vi_tri_ten]
-ten_mien = email[vi_tri_ten + 1:]
-print("ten nguoi dung la:",ten_nguoi_dung)
-print("ten mien la:",ten_mien)
+email = input("khai bao email:").strip().lower()
+if "@" in email:
+    vi_tri_ten = email.find("@")
+    ten_nguoi_dung = email[:vi_tri_ten]
+    ten_mien = email[vi_tri_ten + 1:]
+    print(f"ten nguoi dung la:{ten_nguoi_dung}")
+    print(f"ten mien la:{ten_mien}")
+else:
+    print(f"day khong email hop le(vi ko co dau @)")
