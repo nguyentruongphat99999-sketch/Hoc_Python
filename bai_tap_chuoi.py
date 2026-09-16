76d43279a0535c7e526d61966aa307d62d9a45f3
Nguyễn Trường Phát <nguyentruongphat99999@gmail.com>
2026-09-16 21:19:04 +0700

    cai tien cong phap lan nay them dong lap while

--- bai_tap_chuoi.py
+++ bai_tap_chuoi.py
@@ -1,12 +1,11 @@
-email = input("khai bao email:").strip().lower()
-if "@" in email:
-    vi_tri_ten = email.find("@")
-    ten_nguoi_dung = email[:vi_tri_ten]
-    ten_mien = email[vi_tri_ten + 1:]
-    if "." in ten_mien:
-        print(f"ten mien la:{ten_mien}")
-        print(f"ten nguoi dung la:{ten_nguoi_dung}")
-    else:
-        print("ten mien ko hop le(vi thieu dau .)")
-else:
-    print(f"day khong email hop le(vi ko co dau @)")
+while True:
+    email = input("khai bao email:").strip().lower()
+    if "@" in email:
+        vi_tri_ten = email.find("@")
+        ten_nguoi_dung = email[:vi_tri_ten]
+        ten_mien = email[vi_tri_ten + 1:]
+        if "." in ten_mien:
+            print(f"ten mien la:{ten_mien}")
+            print(f"ten nguoi dung la:{ten_nguoi_dung}")
+            break
+    print("email ko hop le vui long nhap lai(vui long kiem tra coi co @ va .) \n")
