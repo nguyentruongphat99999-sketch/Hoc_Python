b3b30e961289364c832ee392b0dabf23abe62b62
Nguyễn Trường Phát <nguyentruongphat99999@gmail.com>
2026-09-16 21:06:19 +0700

    dai thanh cong phap if long if

--- bai_tap_chuoi.py
+++ bai_tap_chuoi.py
@@ -3,7 +3,10 @@ if "@" in email:
     vi_tri_ten = email.find("@")
     ten_nguoi_dung = email[:vi_tri_ten]
     ten_mien = email[vi_tri_ten + 1:]
-    print(f"ten nguoi dung la:{ten_nguoi_dung}")
-    print(f"ten mien la:{ten_mien}")
+    if "." in ten_mien:
+        print(f"ten mien la:{ten_mien}")
+        print(f"ten nguoi dung la:{ten_nguoi_dung}")
+    else:
+        print("ten mien ko hop le(vi thieu dau .)")
 else:
     print(f"day khong email hop le(vi ko co dau @)")
