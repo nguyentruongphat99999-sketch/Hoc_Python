b8c45284e3f048e77ec9c44e2e48bf3910c5d4e0
Nguyễn Trường Phát <nguyentruongphat99999@gmail.com>
2026-09-21 09:18:27 +0700

    new

--- bai_tap_ham.py
+++ bai_tap_ham.py
@@ -1,26 +1,21 @@
-def kiem_tra_email():
-    so_lan_sai = 0
-    so_lan_nhap_con_lai = 3
-    while True:
-        email = input("khai bao email:").strip().lower()
-        if "@" in email:
-            vi_tri_ten = email.find("@")
-            ten_nguoi_dung = email[:vi_tri_ten]
-            ten_mien = email[vi_tri_ten + 1:]
-            if "." in ten_mien:
-                print(f"ten mien la:{ten_mien}")
-                print(f"ten nguoi dung la:{ten_nguoi_dung}")
-                return True
-        so_lan_sai = so_lan_sai+1
-        so_lan_nhap_con_lai= so_lan_nhap_con_lai-1
-        print(f"email ko hop le vui long nhap lai(vui long kiem tra coi co @ va .) ban con {so_lan_nhap_con_lai} lan nhap  \n")
-        if so_lan_sai == 3:
-            print("tai khoan cua ban da bi khoa")
-            return False
-print ("=== chuong chinh xac thuc email ===")
-ket_qua = kiem_tra_email()
-if ket_qua == True:
-    print("-> chuc mung ban da dang nhap he thong thanh cong")
-else:
-    print("-> rat tiet dang nhap that bai do sai qua nhieu lan")
-    
+def kiem_tra_email(email_nhap_vao):
+    if "@" in email_nhap_vao:
+        vi_tri_ten = email_nhap_vao.find("@")
+        ten_mien = email_nhap_vao[vi_tri_ten + 1:]
+        if "." in ten_mien:
+            return True
+    return False
+so_lan_sai=0
+so_lan_nhap_con_lai=3
+while True:
+    email_nguoi_dung = input("khai bao email:").strip().lower()
+    is_valid = kiem_tra_email(email_nguoi_dung)
+    if is_valid == True:
+        print ("ban da dang nhap thanh cong")
+        break
+    so_lan_sai = so_lan_sai+1
+    so_lan_nhap_con_lai= so_lan_nhap_con_lai-1
+    if so_lan_sai==3:
+        print("tai khoan cua ban da bi khoa")
+        break
+    print(f"email ko hop le vui long nhap lai(vui long kiem tra coi co @ va .) ban con {so_lan_nhap_con_lai} lan nhap  \n")
